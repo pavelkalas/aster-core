be1841d6330eec1c105d5392242b2497f391969b
Pavel Kalaš (Floxen) <pavelkalas2048@seznam.cz>
2026-07-13 21:56:54 +0200

    AsterFS: přidána deklarace asterfs_sync do veřejného rozhraní

--- include/storage.h
+++ include/storage.h
@@ -35,6 +35,7 @@ int asterfs_remove_dir(const char *name);
 int asterfs_write_file(const char *name, const u8 *data, u16 len);
 int asterfs_read_file(const char *name, u8 *out, u16 max_len);
 int asterfs_get_type(const char *name);
+int asterfs_sync(void);
 void asterfs_list(void (*cb)(const char *name, u8 is_dir, u16 size));
 void asterfs_list_dir(const char *path, void (*cb)(const char *name, u8 is_dir, u16 size));
 
