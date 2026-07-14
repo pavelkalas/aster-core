2e796fdacce0a64ba93d118e31234673cfba5b98
Pavel Kalaš (Floxen) <pavelkalas2048@seznam.cz>
2026-07-14 11:30:17 +0200

    Upravena příkladová aplikace, výpis všech barev v terminále

--- sysapps/helloworld.c
+++ sysapps/helloworld.c
@@ -1,5 +1,13 @@
 #include "aster_api.h"
+#include "display.h"
+#include "printk.h"
 
 void init(void) {
-    aster_api_print("Hello, world!");
+    for (int col = 0x01; col <= 0x0F; col++) {
+        display_set_color(col, 0x00);
+        printk("Hello, world! (0x0%d)\n", col);
+    }
+
+    aster_api_print("\n");
+    display_set_color(0x0F, 0x00);
 }
