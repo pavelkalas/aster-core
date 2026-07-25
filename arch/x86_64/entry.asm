bb49a9f1a9aa2c6e0f74cad2151bdd94ab46928e
Pavel Kalaš <pavelkalas2048@seznam.cz>
2026-07-26 01:39:34 +0200

    entry.asm: přidáno nulování BSS sekce při startu kernelu

--- arch/x86_64/entry.asm
+++ arch/x86_64/entry.asm
@@ -13,9 +13,10 @@
 ;  * Co se děje:
 ;  *   1. CLI – zakáže přerušení (ještě nemáme IDT)
 ;  *   2. Napíše 'K' do VGA paměti na znamení, že kernel běží
-;  *   3. Nastaví RSP na stack_top (16 KB zásobník v BSS)
-;  *   4. Zavolá kmain() – hlavní C funkci jádra
-;  *   5. Pokud kmain vrátí, vstoupí do nekonečné HLT smyčky
+;  *   3. Vyčistí BSS sekci (nulování)
+;  *   4. Nastaví RSP na stack_top (16 KB zásobník v BSS)
+;  *   5. Zavolá kmain() – hlavní C funkci jádra
+;  *   6. Pokud kmain vrátí, vstoupí do nekonečné HLT smyčky
 ;  */
 
 [bits 64]
@@ -23,10 +24,22 @@
 section .text.boot
 global _start
 extern kmain
+extern __bss_start
+extern __bss_end
 
 _start:
     cli                              ; zakázat přerušení (IDT není hotová)
     mov word [0xB800A], 0x0F4B       ; vypsat "K" do VGA (důkaz, že kernel běží)
+
+    ; Vyčistit BSS sekci (nulování)
+    mov rdi, __bss_start
+    mov rcx, __bss_end
+    sub rcx, rdi
+    shr rcx, 3                       ; počet qwordů
+    xor rax, rax
+    cld
+    rep stosq
+
     mov rsp, stack_top               ; nastavit stack pointer
     call kmain                       ; předat řízení C jádru
 .halt:
@@ -37,4 +50,3 @@ align 16
 stack_bottom:
     resb 16384                       ; 16 KiB zásobník
 stack_top:
-
