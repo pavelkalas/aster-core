d4b84b9897d51eb8f1a133d451e6bdf45a75a6e9
Pavel Kalaš <pavelkalas2048@seznam.cz>
2026-08-01 02:56:16 +0200

    ring3: přidal jsem hlavičku pro návrat z user modu

--- include/ring3.h
+++ include/ring3.h
@@ -14,8 +14,33 @@
 
 #include "types.h"
 
+/** Softwarovy interrupt pouzity pro navrat z ring 3 do kernelu. */
+#define RING3_RETURN_VECTOR 129
+
 /** Inicializace ring 3 (TSS, syscall MSR). */
 void ring3_init(void);
+
+/**
+ * Spusti sysapp vstupni funkci v ring 3 a po dokonceni se vrati do kernelu.
+ *
+ * @param entry Ukazatel na vstupni funkci sysapp (void (*)(void))
+ * @return      0 pri uspechu, jinak zaporna chyba
+ */
+int ring3_run_sysapp(void (*entry)(void));
+
+/**
+ * Obsluha user interruptu pro navrat z ring 3 do kernelu.
+ *
+ * @return 1 pokud byl vektor zpracovan jako ring3 navrat, jinak 0.
+ */
+int ring3_handle_return_vector(u64 vector,
+                               u64 cs,
+                               u64 *cs_out,
+                               u64 *rip,
+                               u64 *rsp,
+                               u64 *rflags,
+                               u64 *ss,
+                               u64 *rbp);
 
 /**
  * Vstoupí do ring 3 (uživatelský mód) na zadané adrese.
